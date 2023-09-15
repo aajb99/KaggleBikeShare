40eb80415ef907d01635f1bd28ac54b406e9d06c
Aaron Brown <aaron.jacob.brown@gmail.com>
2023-09-15 14:49:31 -0600

    update for IC4

--- BikeShareAnalysis.R
+++ BikeShareAnalysis.R
@@ -1,6 +1,6 @@
 library(tidyverse)
-install.packages('tidymodels')
-install.packages('tidyverse')
+#install.packages('tidymodels')
+#install.packages('tidyverse')
 library(tidymodels)
 library(vroom)
 install.packages('DataExplorer')
@@ -20,8 +20,8 @@ data1$weather[data1$weather == 4] <- 3 # change value of 4 for Weather to 3
 
 factor_cols <- c(2:5)
 
-data1 <- data1 %>%
-  mutate(across(factor_cols, as.factor)) # convert factor variables
+# data1 <- data1 %>%
+#   mutate(across(factor_cols, as.factor)) # convert factor variables
 
 # str(data1)
 
@@ -30,19 +30,53 @@ data1 <- data1 %>%
 
 view(data1)
 
-###############################################################################
+################################################################################
 
 rFormula <- count ~ .
 
 my_recipe <- recipe(rFormula, data = data1) %>% # set model formula and dataset
+  step_mutate(across(factor_cols, as.factor)) %>%
   step_time(datetime, features = 'hour') %>% # get hours
   step_dummy(all_nominal_predictors()) # get dummy variables
 
 prepped_recipe <- prep(my_recipe) # preprocessing new data
 baked_data1 <- bake(prepped_recipe, new_data = data1)
 
-factor_cols2 <- c(8:14)
-baked_data1 <- baked_data1 %>%
-  mutate(across(factor_cols2, as.factor)) # Convert dummy variables into factors
+# factor_cols2 <- c(8:14)
+# baked_data1 <- baked_data1 %>%
+#   mutate(across(factor_cols2, as.factor)) # Convert dummy variables into factors
 
 view(baked_data1[1:10,])
+
+
+################################################################################
+# Linear Regression
+
+my_mod <- linear_reg() %>% # Type of Model
+  set_engine("lm") # R function to use
+
+bike_workflow <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(my_mod) %>%
+  fit(data = data1) # Fit the workflow
+
+data_test <- vroom("test.csv") # grab testing data
+
+data_test$weather <- data_test$weather[data_test$weather == 4] <- 3
+  
+
+bike_predictions <- predict(bike_workflow,
+                            new_data = data_test)
+
+bike_predictions <- bike_predictions %>%
+  mutate(.pred = ifelse(bike_predictions$.pred < 0, 0, bike_predictions$.pred))
+
+
+bike_predictions <- cbind(data_test$datetime, bike_predictions)
+
+colnames(bike_predictions)[1] = "datetime"
+colnames(bike_predictions)[2] = "count"
+
+bike_predictions$datetime <- as.character(format(bike_predictions$datetime))
+
+vroom_write(bike_predictions, "bike_predictions.csv", delim = ",")
